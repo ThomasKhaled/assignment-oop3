f825dfe9147866ed0ccec06448e29502821ddd88
Thomas <thomaskhaled01@gmail.com>
2019-04-19 00:20:53 +0200

    sras

--- G_5012_2017170117_ThomasKhaledMounir_2017170091_AndrewAwni/src/MainFrame.java
+++ G_5012_2017170117_ThomasKhaledMounir_2017170091_AndrewAwni/src/MainFrame.java
@@ -1,15 +1,18 @@
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+import javax.swing.Action;
+import javax.swing.table.DefaultTableModel;
 
 public class MainFrame extends javax.swing.JFrame {
 
-    
     public MainFrame() {
         initComponents();
         setName("Company");
+        jPanel1.setVisible(false);
     }
 
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -21,6 +24,19 @@ public class MainFrame extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jPanel1 = new javax.swing.JPanel();
+        jComboBox1 = new javax.swing.JComboBox();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        idText = new javax.swing.JTextField();
+        nameText = new javax.swing.JTextField();
+        department = new javax.swing.JTextField();
+        salaryText = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        fullPartText = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -33,6 +49,11 @@ public class MainFrame extends javax.swing.JFrame {
         jTextField2.setText("jTextField2");
 
         jButton1.setText("Add Employee");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -47,26 +68,112 @@ public class MainFrame extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(jTable1);
 
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Full Time", "Part Time"}));
+
+        jLabel3.setText("ID");
+
+        jLabel4.setText("Name");
+
+        jLabel5.setText("Department");
+
+        jLabel6.setText("Salary");
+
+        idText.setText("jTextField3");
+
+        nameText.setText("jTextField3");
+
+        department.setText("jTextField3");
+
+        salaryText.setText("jTextField3");
+
+        jLabel7.setText("jlabel");
+
+        fullPartText.setText("jTextField3");
+
+        jButton2.setText("Add");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton2)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel6)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel3)
+                                .addComponent(jLabel4)
+                                .addComponent(jLabel5))
+                            .addGap(18, 18, 18)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(salaryText, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(department, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(nameText, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(idText, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jLabel7)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addGap(75, 75, 75)
+                            .addComponent(fullPartText, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(idText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(nameText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(department, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(salaryText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(fullPartText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addContainerGap(157, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jButton1))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addGap(36, 36, 36)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField2)
-                            .addComponent(jTextField1))))
-                .addGap(217, 217, 217))
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 584, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1)
+                                    .addComponent(jLabel2))
+                                .addGap(36, 36, 36)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 179, Short.MAX_VALUE)
+                                    .addComponent(jTextField1)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(jButton1)))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -84,12 +191,92 @@ public class MainFrame extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(240, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-   
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        jPanel1.setVisible(true);
+        int id = Integer.parseInt(String.valueOf(idText.getText()));
+        int salary = Integer.parseInt(String.valueOf(salaryText.getText()));
+        String name = nameText.getText();
+;        String dep = department.getText();
+        int fullPartDependant = Integer.parseInt(String.valueOf(fullPartText.getText()));
+        
+        String kindOfEmployee = (String) jComboBox1.getSelectedItem();
+        if (kindOfEmployee.equals("Full Time")) {
+                        jLabel7.setText("Insurance_no");
+        }
+        else if (kindOfEmployee.equals("Part Time")) {
+                        jLabel7.setText("Work Hours");
+        }
+        DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
+
+        if (addedEmployees == 0) {
+            companyName = jTextField1.getText();
+            employeesCount = Integer.parseInt(jTextField2.getText());
+            company = new Company(companyName);
+        }
+        if (addedEmployees > employeesCount) {
+            /*TO DO: 
+             1-create add employee form and fetch data from it
+             2-update the table
+             3-increment addedEmployees
+             */
+            jButton2.setAction(new Action() {
+
+                @Override
+                public Object getValue(String key) {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public void putValue(String key, Object value) {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public void setEnabled(boolean b) {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public boolean isEnabled() {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public void addPropertyChangeListener(PropertyChangeListener listener) {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public void removePropertyChangeListener(PropertyChangeListener listener) {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    
+                 
+
+                        Employee fullTime = new FullTimeEmployee(fullPartDependant, id, name, dep, salary);
+                        model.addRow(new Object[]{fullTime.getEmployeeInfo()});
+                      if (kindOfEmployee.equals("Part Time")) {
+                        Employee partTime = new FullTimeEmployee(fullPartDependant, id, name, dep, salary);
+
+                    }
+                }
+            });
+
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -121,104 +308,75 @@ public class MainFrame extends javax.swing.JFrame {
             }
         });
 
-        jButton1.addActionListener(new ActionListener(){
-        
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(addedEmployees == 0)
-                {
-                    companyName = jTextField1.getText();
-                    employeesCount = Integer.parseInt(jTextField2.getText());
-                    company = new Company(companyName);
-                }
-                if(addedEmployees > employeesCount)
-                {
-                    /*TO DO: 
-                        1-create add employee form and fetch data from it
-                        2-update the table
-                        3-increment addedEmployees
-
-                    */
-
-                }
-                
-            }
-        });
-
-
-
-
-
-        
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     private static Company company;
     private static String companyName;
     private static int employeesCount;
     private static int addedEmployees = 0;
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private static javax.swing.JButton jButton1;
-    private static javax.swing.JLabel jLabel1;
-    private static javax.swing.JLabel jLabel2;
-    private static javax.swing.JScrollPane jScrollPane1;
-    private static javax.swing.JTable jTable1;
-    private static javax.swing.JTextField jTextField1;
-    private static javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField department;
+    private javax.swing.JTextField fullPartText;
+    private javax.swing.JTextField idText;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField nameText;
+    private javax.swing.JTextField salaryText;
     // End of variables declaration//GEN-END:variables
 }
 
+class Company {
 
-class Company{
     private String m_name;
     private Employee[] m_Employees;
-    
-    public Company(String name){
+
+    public Company(String name) {
         m_name = name;
     }
-    public void insertEmployees(Employee [] employees){
-       //employees[m_Employees.getEmployeeInfo().m_id] = m_Employees.getEmployeeInfo();
-       m_Employees = employees;
+
+    public void insertEmployees(Employee[] employees) {
+        //employees[m_Employees.getEmployeeInfo().m_id] = m_Employees.getEmployeeInfo();
+        m_Employees = employees;
     }
-    
 
 }
 
-abstract class Employee{
-    
+abstract class Employee {
+
     protected final int m_id;
     protected String m_name;
     protected String m_department;
     protected int m_salary;
-    
+
     public Employee(int m_id, String m_name, String m_department, int m_salary) {
         this.m_id = m_id;
         this.m_name = m_name;
         this.m_department = m_department;
         this.m_salary = m_salary;
     }
-    
+
     public abstract Employee getEmployeeInfo();
-  
+
 }
 
-class PartTimeEmployee extends Employee{
+class PartTimeEmployee extends Employee {
+
     private int m_workHours;
 
-    public PartTimeEmployee(int workHours,int id, String name, String department, int salary) {
+    public PartTimeEmployee(int workHours, int id, String name, String department, int salary) {
         super(id, name, department, salary);
         this.m_workHours = workHours;
     }
@@ -227,14 +385,14 @@ class PartTimeEmployee extends Employee{
     public Employee getEmployeeInfo() {
         return this;
     }
-    
-    
+
 }
 
-class FullTimeEmployee extends Employee{
+class FullTimeEmployee extends Employee {
+
     private int m_insuranceNumber;
 
-    public FullTimeEmployee(int insuranceNumber,int id, String name, String department, int salary) {
+    public FullTimeEmployee(int insuranceNumber, int id, String name, String department, int salary) {
         super(id, name, department, salary);
         this.m_insuranceNumber = insuranceNumber;
     }
@@ -243,6 +401,5 @@ class FullTimeEmployee extends Employee{
     public Employee getEmployeeInfo() {
         return this;
     }
-    
-    
+
 }
